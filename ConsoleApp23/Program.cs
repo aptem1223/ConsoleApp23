9c40b29a0fb8bb4996c724dfe3887c33624df730
Аня <@IDEAPAD>
2024-11-11 23:54:41 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int[] numbers = { 5, 10, 15, 20, 25, 30, 35, 40 };
+            //Console.WriteLine("Исходный массив:");
+            //foreach (int num in numbers)
+            //{
+            //    Console.Write(num + " ");
+            //}
+            //for (int i = 1; i <= 3; i++)
+            //{
+            //    numbers[i] = 0;
+            //}
+            //Console.WriteLine("\nИзмененный массив:");
+            //foreach (int num in numbers)
+            //{
+            //    Console.Write(num + " ");
+            //}
+            //Console.ReadLine();
+
+            //int[] numbers = { 1,3,5,7,9,11,13,15};
+            //Console.WriteLine("Исходный массив:");
+            //foreach (int num in numbers)
+            //{
+            //    Console.Write(num + " ");
+            //}
+            //for (int i = 1; i <= 3; i++)
+            //{
+            //    numbers[i] = 0;
+            //}
+            //int newSize = 5;
+            //int[] resizedArray = new int[newSize];
+            //Array.Copy(numbers, resizedArray, newSize);
+            //Console.WriteLine("\nИзмененный массив (размер: " + newSize + "):");
+            //foreach (int num in resizedArray)
+            //{
+            //    Console.Write(num + " ");
+            //}
+            //Console.ReadLine();
+
+            
+            //Random rand = new Random();
+            //int[] numbers = new int[10];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    numbers[i] = rand.Next(-5, 6); 
+            //}
+            //Console.WriteLine("Массив:");
+            //foreach (int num in numbers)
+            //{
+            //    Console.Write(num + " ");
+            //}
+            //int index = -1;  
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] < 0)
+            //    {
+            //        index = i;
+            //        break; 
+            //    }
+            //}
+            //if (index != -1)
+            //{
+            //    Console.WriteLine("\nИндекс первого отрицательного элемента: " + index);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("\nОтрицательные элементы в массиве отсутствуют.");
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
